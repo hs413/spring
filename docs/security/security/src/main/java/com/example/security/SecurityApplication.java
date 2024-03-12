37eef9ea46e480d48927b5efd72c56df80321e9a
rtg <rtg1539@naver.com>
2024-03-12 21:18:24 +0900

    docs secure filter 테스트2

--- docs/security/security/src/main/java/com/example/security/SecurityApplication.java
+++ docs/security/security/src/main/java/com/example/security/SecurityApplication.java
@@ -2,12 +2,27 @@ package com.example.security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Collection;
 
 @SpringBootApplication
 public class SecurityApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SecurityApplication.class, args);
+
+		// context holder test - start
+		SecurityContext context = SecurityContextHolder.getContext();
+		Authentication authentication = context.getAuthentication();
+		String username = authentication.getName();
+		Object principal = authentication.getPrincipal();
+		Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+		System.out.println(username);
+		// context holder test - end
 	}
 
 }
--- docs/security/security/src/main/java/com/example/security/SecurityConfig.java
+++ docs/security/security/src/main/java/com/example/security/SecurityConfig.java
@@ -4,15 +4,30 @@ import com.example.security.filter.TenantFilter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.TestingAuthenticationToken;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.intercept.AuthorizationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    @Bean
+    public String contextHolder() {
+        SecurityContext context = SecurityContextHolder.createEmptyContext();
+        Authentication authentication =
+            new TestingAuthenticationToken("username", "password", "ROLE_USER");
+        context.setAuthentication(authentication);
+
+        SecurityContextHolder.setContext(context);
+        return "";
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         System.out.println("filter");
