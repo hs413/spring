68d3a7cdffead641dfd7914e445e253d67ec750a
rtg <rtg1539@naver.com>
2024-05-01 23:41:49 +0900

    Spring container로 전환

--- demo2/src/main/java/com/example/demo2/OrderApp.java
+++ demo2/src/main/java/com/example/demo2/OrderApp.java
@@ -5,12 +5,18 @@ import com.example.demo2.member.Member;
 import com.example.demo2.member.MemberService;
 import com.example.demo2.order.Order;
 import com.example.demo2.order.OrderService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+//        OrderService orderService = appConfig.orderService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
 
         long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
