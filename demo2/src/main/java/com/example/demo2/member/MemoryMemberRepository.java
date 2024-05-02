4c773c4dad38e590d5aa91ce3d18a2017687efe5
rtg <rtg1539@naver.com>
2024-05-02 11:59:05 +0900

    request scope

--- demo2/src/main/java/com/example/demo2/member/MemoryMemberRepository.java
+++ demo2/src/main/java/com/example/demo2/member/MemoryMemberRepository.java
@@ -1,11 +1,14 @@
 package com.example.demo2.member;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @Component
+@RequiredArgsConstructor
 public class MemoryMemberRepository implements MemberRepository {
     private static Map<Long, Member> store = new HashMap<>();
     @Override
