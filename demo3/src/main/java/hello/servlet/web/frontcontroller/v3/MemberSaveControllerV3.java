8576171f6899016f66daaa36a88eba2bc51a615e
rtg <rtg1539@naver.com>
2024-05-03 08:37:27 +0900

    front controller v3

---
+++ demo3/src/main/java/hello/servlet/web/frontcontroller/v3/MemberSaveControllerV3.java
@@ -0,0 +1,28 @@
+package hello.servlet.web.frontcontroller.v3;
+import hello.servlet.domain.member.Member;
+import hello.servlet.domain.member.MemberRepository;
+import hello.servlet.web.frontcontroller.ModelView;
+import hello.servlet.web.frontcontroller.MyView;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.Map;
+
+public class MemberSaveControllerV3 implements ControllerV3 {
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    public ModelView process(Map<String, String> paramMap) {
+        String username = paramMap.get("username"); // paramMap에서 필요한 요청 파라미터 조회
+        int age = Integer.parseInt(paramMap.get("age"));
+
+        Member member = new Member(username, age);
+        memberRepository.save(member);
+
+        ModelView mv = new ModelView("save-result");
+        mv.getModel().put("member", member); // 모델에 뷰에서 필요한 객체를 담고 반환
+        return mv;
+    }
+}
