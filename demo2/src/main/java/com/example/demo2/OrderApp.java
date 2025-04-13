054afdae6d1612acdeacccb0f5ee8a2c502a9c5d
rtg <rtg1539@naver.com>
2025-04-13 23:24:49 +0900

    AppConfig를 사용한 관심사의 분리

--- demo2/src/main/java/com/example/demo2/OrderApp.java
+++ demo2/src/main/java/com/example/demo2/OrderApp.java
@@ -3,15 +3,14 @@ package com.example.demo2;
 import com.example.demo2.member.Grade;
 import com.example.demo2.member.Member;
 import com.example.demo2.member.MemberService;
-import com.example.demo2.member.MemberServiceImpl;
 import com.example.demo2.order.Order;
 import com.example.demo2.order.OrderService;
-import com.example.demo2.order.OrderServiceImpl;
 
 public class OrderApp {
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
-        OrderService orderService = new OrderServiceImpl();
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberService();
+        OrderService orderService = appConfig.orderService();
 
         long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
