2a523cdc768ee3673e61082122b471e6ddb534bf
rtg <rtg1539@naver.com>
2025-05-26 10:28:04 +0900

    데이터 접근 - JPA

--- 10-item-service-db/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ 10-item-service-db/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -1,6 +1,7 @@
 package hello.itemservice;
 
 import hello.itemservice.config.JdbcTemplateV3Config;
+import hello.itemservice.config.JpaConfig;
 import hello.itemservice.config.MyBatisConfig;
 import hello.itemservice.repository.ItemRepository;
 import lombok.extern.slf4j.Slf4j;
@@ -17,7 +18,8 @@ import javax.sql.DataSource;
 //@Import(MemoryConfig.class)
 //@Import(JdbcTemplateV1Config.class)
 //@Import(JdbcTemplateV3Config.class)
-@Import(MyBatisConfig.class)
+//@Import(MyBatisConfig.class)
+@Import(JpaConfig.class)
 //@SpringBootApplication(scanBasePackages = "hello.itemservice.web")
 @SpringBootApplication
 public class ItemServiceApplication {
