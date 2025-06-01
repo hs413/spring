edfef9c20243e5ed96a685fc65fcfbe094eba685
rtg <rtg1539@naver.com>
2025-06-01 14:08:46 +0900

    자바 람다

---
+++ 18-lambda/src/ex3/RefOfIntanceMethod.java
@@ -0,0 +1,38 @@
+package ex3;
+
+interface A {
+    void abc();
+}
+
+class B {
+    void bcd() {
+        System.out.println("메서드");
+    }
+}
+
+public class RefOfIntanceMethod {
+    public static void main(String[] args) {
+        // #1. 인스턴스 메서드 참조
+        A a1 = new A() {
+            @Override
+            public void abc() {
+                B b = new B();
+                b.bcd();
+            }
+        };
+
+        // #2. 람다식
+        A a2 = () -> {
+            B b = new B();
+            b.bcd();
+        };
+
+        // #3. 인스턴스 참조
+        B b = new B();
+        A a3 = b::bcd;
+
+        a1.abc();
+        a2.abc();
+        a3.abc();
+    }
+}
