ca68dda0670360e909ddd8aa0eb15aacfb7ad044
rtg <rtg2541@gmail.com>
2025-11-09 20:13:50 +0900

    ConnectionMaker 인터페이스

---
+++ tb/01-dependency/src/main/java/user/dao/DConnectionMaker.java
@@ -0,0 +1,16 @@
+package user.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DConnectionMaker implements ConnectionMaker {
+
+    @Override
+    public Connection makeNewConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection c = DriverManager.getConnection("jdbc:mysql://localhost:3306/tb", "root", "");
+
+        return c;
+    }
+}
